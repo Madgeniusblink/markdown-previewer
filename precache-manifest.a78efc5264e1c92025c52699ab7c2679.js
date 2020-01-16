709bc5eeffbfbefebcb537725c11564e3968908b
Madgeniusblink <madgeniusblink@gmail.com>
2020-01-15 22:48:58 -0700

    Updates

---
+++ precache-manifest.a78efc5264e1c92025c52699ab7c2679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d9abc936cbd7fe57fc6f2194a4f350",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "f89ddb2712ff757f5622",
+    "url": "/markdown-previewer/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "3a7be0cb6cdc188b45bd",
+    "url": "/markdown-previewer/static/css/main.a2ec6b4f.chunk.css"
+  },
+  {
+    "revision": "f89ddb2712ff757f5622",
+    "url": "/markdown-previewer/static/js/2.39aa3e36.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/markdown-previewer/static/js/2.39aa3e36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a7be0cb6cdc188b45bd",
+    "url": "/markdown-previewer/static/js/main.6f02cc03.chunk.js"
+  },
+  {
+    "revision": "52d3382a8ff67d86a3dd",
+    "url": "/markdown-previewer/static/js/runtime-main.6a495000.js"
+  }
+]);
